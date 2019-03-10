2de5cf6b536500e56ca11612af93f8c5817a6e74
macos <macos@macoss-Mac.local>
2019-03-10 16:56:42 -0400

    code of login

--- IOS/ViewController.swift
+++ IOS/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+@IBOutlet weak var UserNameOrEmail: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
+    @IBOutlet weak var InvalidUsernameOrEmail: UILabel!
+    
+    @IBOutlet weak var InvalidPassword: UILabel!
+    
+    @IBOutlet weak var LoginMessage: UILabel!
+    
+    @IBOutlet weak var RememberMe: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-
+    @IBAction func LOGIBsumbit(_ sender: UIButton) {
+        InvalidUsernameOrEmail.text = ""
+        InvalidPassword.text = ""
+        LoginMessage.text = ""
+        if UserNameOrEmail.text == "amrit@.com"
+        {
+            if Password.text == "Amrit"
+            {
+                LoginMessage.text = "Login Successful"
+            }
+            else
+            {
+                InvalidPassword.text = "Invalid Password"
+            }
+        }
+        else
+        {
+            InvalidUsernameOrEmail.text = "Invalid UserNameOrEmail"
+        }
+        
+    }
+    
 }
 
